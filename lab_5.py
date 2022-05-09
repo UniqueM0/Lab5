8bc0214d8779eccb83a06a0e1db21edb6e765d35
Unique Orduna <uniqueorduna@uniques-air.attlocal.net>
2022-05-09 15:49:56 -0700

    changed

--- lab_5.py
+++ lab_5.py
@@ -34,25 +34,9 @@ create_dictionary():
     create list = each line from file
     close the file
     create a dict off of the list
-    return the dict
+    return the dict.
+
 
 main()
 """
 
-def main():
-    sentence = input("Enter a sentence: ")
-    dictionary = create_dictionary("Textese.txt")
-    translate(sentence, dictionary)
-def create_dictionary(txt_file):
-    infile = open(txt_file, "r")
-    words = [word.rstrip() for word in infile]
-    infile.close()
-    return dict([word.split(",") for word in words])
-    
-def translate(sentence, dictionary):
-    words = sentence.split()
-    for word in words:
-        print(dictionary.get(word, word), " ", end="")
-
-
-main()
